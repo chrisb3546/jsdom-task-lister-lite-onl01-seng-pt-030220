52849049e8bbbabfe6ffabfd7e5aca71f105633a
chrisb3546 <chrisb3546@yahoo.com>
2020-08-24 10:46:59 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  
+  
+
+  
 });
+
+// function handleSubmit(e){
+//   e.preventDefault
+//   const tasks = document.getElementById('tasks')
+//   let input = 
+//   tasks.innerText = input
+//   // debugger
+//   // let li = document.createElement('li')
+//   // li.innerText = input
+//   // tasks.appendChild(li)
+
+document.addEventListener("submit", appendList)
+
+  function appendList() {
+    preventDefault()
+    let list = document.getElementById('tasks');
+    let listItem = document.createElement('li');
+    let newTask = document.getElementById("new-task-description")
+    list.appendChild(listItem)
+    listItem.appendChild(document.createTextNode(newTask.value));
+    
+}
+
+
